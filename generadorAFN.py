1edfa432d27a24306e12f72af2288e6866facf3d
Ana <dia151378@uvg.edu.gt>
2020-03-19 03:12:11 -0600

    AFD parentesis fixed

--- generadorAFN.py
+++ generadorAFN.py
@@ -1,7 +1,11 @@
 from transformacion import Transformacion 
-from graphviz import Digraph
+from collections import defaultdict
+from graphviz import Digraph , render
 global array_last
-
+alphabet = [chr(i) for i in range(ord('A'), ord('Z') + 1)] + \
+    [chr(i) for i in range(ord('a'), ord('z') + 1)] + \
+    [chr(i) for i in range(ord('0'), ord('9') + 1)]
+epsilon = 'ε'
 
 def graficar_AFD(transformacion_final, inicial_final):
     f = Digraph('finite_state_machine', filename='./Automatas_Graficados/afn_to_afd')
@@ -13,9 +17,28 @@ def graficar_AFD(transformacion_final, inicial_final):
     for i in range(len(transformacion_final)):
         f.edge(str(transformacion_final[i][0]), str(transformacion_final[i][2]), label= str(transformacion_final[i][1]))
     f.view()
-
+def graficar_AFDVS2(transformacion_final, inicial_final):
+    f = Digraph('finite_state_machine', filename='./Automatas_Graficados/afn_to_afd2')
+    f.attr(rankdir='LR', size='8,5')
+    f.attr('node', shape='doublecircle')
+    for i in range(len(inicial_final)):
+        f.node(str(inicial_final[i][1]))
+    f.attr('node', shape='circle')
+    for i in range(len(transformacion_final)):
+        f.edge(str(transformacion_final[i][0]), str(transformacion_final[i][2]), label= str(transformacion_final[i][1]))
+    f.view()
 def graficar_AFN(transformacion_final, inicial_final):
     f = Digraph('finite_state_machine', filename='./Automatas_Graficados/afn')
+    f.attr(rankdir='LR', size='8,5')
+    f.attr('node', shape='doublecircle')
+    for i in range(len(inicial_final)):
+        f.node(str(inicial_final[i][1]))
+    f.attr('node', shape='circle')
+    for i in range(len(transformacion_final)):
+        f.edge(str(transformacion_final[i][0]), str(transformacion_final[i][2]), label= str(transformacion_final[i][1]))
+    f.view()
+def graficar_AFNVS2(transformacion_final, inicial_final):
+    f = Digraph('finite_state_machine', filename='./Automatas_Graficados/afn_02')
     f.attr(rankdir='LR', size='8,5')
     f.attr('node', shape='doublecircle')
     for i in range(len(inicial_final)):
@@ -252,5 +275,214 @@ class afn(object):
 		arreglo.append(self.initialState)
 		arreglo.append(self.finalState)
 		return arreglo
+class AFN_ESTADO:
+
+    def __init__(self, simbolo = set([])):
+        self.estados = set()
+        self.simbolo = simbolo    
+        self.transiciones = defaultdict(defaultdict)
+        self.init_estado = None
+        self.estado_final = []
+    
+    def inicio_final(self):
+        arreglo = []
+        arreglo.append(self.init_estado)
+        arreglo.append(self.estado_final[0])
+        arreglo_final2 = []
+        arreglo_final2.append(arreglo)
+        print(arreglo_final2)
+        return arreglo_final2
+
+    def transformacion_vs2(self):
+        arreglofinal = []
+        for estado_anterior, nuevo_estados in self.transiciones.items():
+            subarray = []
+            for estado in nuevo_estados:
+                variable_pasada = ''
+                for s in nuevo_estados[estado]:
+                    variable_pasada += s + '|'
+                    #arreglofinal.append(subarray)
+                    #('s' + str(estado_anterior), 's' + str(estado), label = variable_pasada[:-1])
+                print("Resultado02", str(estado_anterior) + "," + variable_pasada[:-1] + ","+ str(estado))
+                array = []
+                array.append(estado_anterior)
+                array.append(variable_pasada[:-1])
+                array.append(estado)
+                arreglofinal.append(array)                    
+        print("TRANSICION", arreglofinal)
+        print("final",arreglofinal, self.init_estado, self.estado_final)
+        return arreglofinal
+       
+    def Marcar_Inicio(self, estado):
+        self.init_estado = estado
+        self.estados.add(estado)
+
+    def Agregar_Final(self, estado):
+        if isinstance(estado, int):
+            estado = [estado]
+        for s in estado:
+            if s not in self.estado_final:
+                self.estado_final.append(s)
+
+    def Agregar_Transicion(self, estado_anterior, nuevo_estado, expresion):  
+        if isinstance(expresion, str):
+            expresion = set([expresion])
+        self.estados.add(estado_anterior)
+        self.estados.add(nuevo_estado)
+        if estado_anterior in self.transiciones and nuevo_estado in self.transiciones[estado_anterior]:
+            self.transiciones[estado_anterior][nuevo_estado] = \
+            self.transiciones[estado_anterior][nuevo_estado].union(expresion)
+        else:
+            self.transiciones[estado_anterior][nuevo_estado] = expresion
+
+    def Agregar_Transicion_dict(self, transiciones):  
+        for estado_anterior, nuevo_estados in transiciones.items():
+            for estado in nuevo_estados:
+                self.Agregar_Transicion(estado_anterior, estado, nuevo_estados[estado])
+                print("mostrar transicion", str(self.Agregar_Transicion(estado_anterior, estado, nuevo_estados[estado])) )
+
+    def Construir_Por_Num(self, ninicio):
+   
+        translations = {}
+        for i in self.estados:
+            translations[i] = ninicio
+            ninicio += 1
+        re_construir = AFN_ESTADO(self.simbolo)
+        re_construir.Marcar_Inicio(translations[self.init_estado])
+        re_construir.Agregar_Final(translations[self.estado_final[0]])
+        
+        for estado_anterior, nuevo_estados in self.transiciones.items():
+            for estado in nuevo_estados:
+                re_construir.Agregar_Transicion(translations[estado_anterior], translations[estado], nuevo_estados[estado])
+        print("newbuild", [re_construir, ninicio] )
+        return [re_construir, ninicio]
 
 
+class Exp_AFNVS2:
+
+    def __init__(self, expreg):
+        self.expreg = expreg
+        self.ConstruirSecAFN()
+
+    def InEnd(self):
+        variable = self.nfa.inicio_final()
+        return variable
+    def FunctionsNFA(self):
+        variable = self.nfa.transformacion_vs2()
+        return variable
+    
+    def tomar_prioridad(op):
+        if op == '|':
+            return 1
+        elif op == '·':
+            return 2
+        elif op == '*':
+            return 3
+        else:       
+            return 0
+
+    
+    def unico_caracter(expresion):   
+        estado1 = 1
+        estado2 = 2
+        estructura = AFN_ESTADO(set([expresion]))
+        estructura.Marcar_Inicio(estado1)
+        estructura.Agregar_Final(estado2)
+        estructura.Agregar_Transicion(estado1, estado2, expresion)
+        return estructura
+
+    def union(a, b):  
+        [a, sec] = a.Construir_Por_Num(2)
+        [b, sec2] = b.Construir_Por_Num(sec)
+        estado1 = 1
+        estado2 = sec2
+        unionAFN = AFN_ESTADO(a.simbolo.union(b.simbolo))
+        unionAFN.Marcar_Inicio(estado1)
+        unionAFN.Agregar_Final(estado2)
+        unionAFN.Agregar_Transicion(unionAFN.init_estado, a.init_estado, epsilon)
+        unionAFN.Agregar_Transicion(unionAFN.init_estado, b.init_estado, epsilon)
+        unionAFN.Agregar_Transicion(a.estado_final[0], unionAFN.estado_final[0], epsilon)
+        unionAFN.Agregar_Transicion(b.estado_final[0], unionAFN.estado_final[0], epsilon)
+        unionAFN.Agregar_Transicion_dict(a.transiciones)
+        unionAFN.Agregar_Transicion_dict(b.transiciones)
+        return unionAFN
+
+  
+    def concatenar(a, b):   
+        [a, sec] = a.Construir_Por_Num(1)
+        [b, sec2] = b.Construir_Por_Num(sec)
+        estado1 = 1
+        estado2 = sec2 - 1
+        concatenarAFN = AFN_ESTADO(a.simbolo.union(b.simbolo))
+        concatenarAFN.Marcar_Inicio(estado1)
+        concatenarAFN.Agregar_Final(estado2)
+        concatenarAFN.Agregar_Transicion(a.estado_final[0], b.init_estado, epsilon)
+        concatenarAFN.Agregar_Transicion_dict(a.transiciones)
+        concatenarAFN.Agregar_Transicion_dict(b.transiciones)
+        return concatenarAFN
+
+  
+    def variable_kleene(a):  
+        [a, sec] = a.Construir_Por_Num(2)
+        estado1 = 1
+        estado2 = sec
+        Kleene = AFN_ESTADO(a.simbolo)
+        Kleene.Marcar_Inicio(estado1)
+        Kleene.Agregar_Final(estado2)
+        Kleene.Agregar_Transicion(Kleene.init_estado, a.init_estado, epsilon)
+        Kleene.Agregar_Transicion(Kleene.init_estado, Kleene.estado_final[0], epsilon)
+        Kleene.Agregar_Transicion(a.estado_final[0], Kleene.estado_final[0], epsilon)
+        Kleene.Agregar_Transicion(a.estado_final[0], a.init_estado, epsilon)
+        Kleene.Agregar_Transicion_dict(a.transiciones)
+        return Kleene
+
+    def ConstruirSecAFN(self):
+        caracter = ''
+        exp1 = ''
+        simbolo = set()
+        for token in self.expreg:
+            if token in alphabet:
+                simbolo.add(token)
+            if token in alphabet or token == '(':
+                if exp1 != '·' and (exp1 in alphabet or exp1 in ['*', ')']):
+                    caracter += '·'
+            caracter += token
+            exp1 = token
+        self.expreg = caracter
+        caracter = ''
+        final_array = []
+        for token in self.expreg:
+            if token in alphabet:
+                caracter += token
+            elif token == '(':
+                final_array.append(token)
+            elif token == ')':
+                while(final_array[-1] != '('):
+                    caracter += final_array[-1]
+                    final_array.pop()
+                final_array.pop()    
+            else:
+                while(len(final_array) and Exp_AFNVS2.tomar_prioridad(final_array[-1]) >= Exp_AFNVS2.tomar_prioridad(token)):
+                    caracter += final_array[-1]
+                    final_array.pop()
+                final_array.append(token)
+        while(len(final_array) > 0):
+            caracter += final_array.pop()
+        self.expreg = caracter
+        self.automata = []
+        for token in self.expreg:
+            if token in alphabet:
+                self.automata.append(Exp_AFNVS2.unico_caracter(token))
+            elif token == '|':
+                b = self.automata.pop()
+                a = self.automata.pop()
+                self.automata.append(Exp_AFNVS2.union(a, b))
+            elif token == '·':
+                b = self.automata.pop()
+                a = self.automata.pop()
+                self.automata.append(Exp_AFNVS2.concatenar(a, b))
+            elif token == '*':
+                a = self.automata.pop()
+                self.automata.append(Exp_AFNVS2.variable_kleene(a))
+        self.nfa = self.automata.pop()
+        self.nfa.simbolo = simbolo
